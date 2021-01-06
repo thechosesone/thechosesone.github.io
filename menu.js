c200d1796099b9447c9ac5102ff05deafa36b807
joseph <josephvitale93@gmail.com>
2021-01-06 20:11:38 +0100

    mobile menu implementado

--- menu.js
+++ menu.js
@@ -1,20 +1,30 @@
 const hamburgerbutton = document.querySelector('#hamburgerbutton');
+const mobilemenu = document.querySelector('#mobilemenu');
 
 console.log("si funciono");
-hamburgerbutton.addEventListener('click', function() {
+hamburgerbutton.addEventListener('click', menutoggle);
+mobilemenu.addEventListener('click', menutoggle);
+
+function menutoggle() {
+
 console.log('click hamburguer');
 if(hamburgerbutton.classList.contains('open')) {
     hamburgerbutton.classList.remove('open');
     hamburgerbutton.classList.add('closed');
+    mobilemenu.classList.remove('menuopen');
+    mobilemenu.classList.add('menuclosed');
     setTimeout(rmclosed, 1000);
     
         function rmclosed() {
             hamburgerbutton.classList.remove('closed');
+            mobilemenu.classList.remove('menuclosed');
     }
 }
 else {
 
     hamburgerbutton.classList.remove('closed');
     hamburgerbutton.classList.add('open');
+    mobilemenu.classList.remove('menuclosed');
+    mobilemenu.classList.add('menuopen');
 }
-});
+}
